bb928e455a749d71e6453d4f664da6886fdbcbd1
Roman <email.rom.melnyk@gmail.com>
2017-07-17 01:13:19 +0200

    SCSS: buttons, grid, lists

--- server/admin.php
+++ server/admin.php
@@ -5,6 +5,15 @@
     <script type="application/javascript" src="js/admin-app.js"></script>
 </head>
 <body onload="App.start();">
-<h1>Admin page</h1>
+<h1>Admin page <span class="selected">selected</span> Admin page</h1>
+<p>Lorem ipsum <span class="selected">selected</span> Lorem ipsum </p>
+<div class="h-list">
+    <span class="button">test</span>
+    <span class="button narrow">narrow</span>
+    <span class="button red">red</span>
+    <span class="button green">green</span>
+    <span class="button blue">blue</span>
+    <span class="button grey">grey</span>
+</div>
 </body>
 </html>
